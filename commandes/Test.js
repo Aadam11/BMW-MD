c7cafa993292d7f89eff4d06a9be74b7150be2c0
Aadam11 <107280979+Aadam11@users.noreply.github.com>
2024-06-10 23:07:59 +0100

    Update Test.js

--- commandes/Test.js
+++ commandes/Test.js
@@ -4,9 +4,9 @@ const { france } = require("../framework/france");
 france({ nomCom: "test", reaction: "⏳", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '𝘽𝙊𝙏 𝙄𝙎 𝙊𝙉𝙇𝙄𝙉𝙀 \n\n ' + "║❒ 𝙏𝙔𝙋𝙀 𝙈𝙀𝙉𝙐 𝙏𝙊 𝙎𝙀𝙀 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎";
-    let d = '                                            ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@ibrahimaitech';
+    let d = '                                            ║❒ Handle https://linktr.ee/ateamupdates';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/16dd2b3823fedec5c8d88.jpg';
+    var img = 'https://telegra.ph/file/a0ed56bf8baed297aaa1b.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
