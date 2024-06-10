bccafb9d7dd16d1dcef88bc8ad9d2d40ba405fcf
Aadam11 <107280979+Aadam11@users.noreply.github.com>
2024-06-10 23:03:17 +0100

    Update Ibrahim.js

--- commandes/Ibrahim.js
+++ commandes/Ibrahim.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "ibrahim", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "ateam", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Yes you called me whats up Ok anyway support me by following my channel and subscribing on my youtube channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
-    let d = '                                     ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@ibrahimaitech';
+    let z = 'Yes you called me whats up Ok anyway support me by following my channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 Follow the 𝘼𝙏𝙀𝘼𝙈 𝙐𝙋𝘿𝘼𝙏𝙀𝙎🔥🤑💰 channel on WhatsApp: https://whatsapp.com/channel/0029VaEeQjhLNSZvnrfrCB17";
+    let d = '                                     ║❒ Handles https://linktr.ee/ateamupdates';
     let varmess = z + d;
     var mp4 = 'https://telegra.ph/file/dec033e457813cd2ff2e3.mp4';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
